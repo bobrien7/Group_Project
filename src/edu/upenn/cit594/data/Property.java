e89ea74653aea9b05787e0e79fb089bd5db01872
Bobby O'Brien <robrien7@seas.upenn.edu>
2020-04-19 13:24:35 -0400

    Updated Property class with applicable fields, constructor, and getters

--- src/edu/upenn/cit594/data/Property.java
+++ src/edu/upenn/cit594/data/Property.java
@@ -1,4 +1,27 @@
 package edu.upenn.cit594.data;
 
 public class Property {
+
+    private double marketValue;
+    private int zipCode;
+    private int totalLivableArea;
+
+    public Property(double marketValue, int zipCode, int totalLivableArea){
+
+        this.marketValue = marketValue;
+        this.zipCode = zipCode;
+        this.totalLivableArea = totalLivableArea;
+    }
+
+    public double getMarketValue() {
+        return marketValue;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public int getTotalLivableArea() {
+        return totalLivableArea;
+    }
 }
