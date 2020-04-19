c926f4ff7144c1529608612b6c524b8a98149996
Bobby O'Brien <robrien7@seas.upenn.edu>
2020-04-19 13:27:03 -0400

    Updated ZipCode class with applicable field, constructor, and getter

---
+++ src/edu/upenn/cit594/data/ZipCode.java
@@ -0,0 +1,14 @@
+package edu.upenn.cit594.data;
+
+public class ZipCode {
+
+    private int population;
+
+    public ZipCode(int population){
+        this.population = population;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+}
