c39df03532e33720e412042794d50a39ff910515
Bobby O'Brien <robrien7@seas.upenn.edu>
2020-04-19 13:16:12 -0400

    Updated ParkingViolation class with applicable fields, constructor, and getter methods

--- src/edu/upenn/cit594/data/ParkingViolation.java
+++ src/edu/upenn/cit594/data/ParkingViolation.java
@@ -2,6 +2,54 @@ package edu.upenn.cit594.data;
 
 public class ParkingViolation {
 
+    private String timestamp;
+    private int fineAmount;
+    private String violationDescription;
+    private int vehicleID;
+    private String vehicleState;
+    private int violationNumber;
+    private int violationZipCode;
 
+    public ParkingViolation(String timestamp, int fineAmount, String violationDescription, int vehicleID, String vehicleState,
+                            int violationNumber, int violationZipCode){
+
+        this.timestamp = timestamp;
+        this.fineAmount = fineAmount;
+        this.violationDescription = violationDescription;
+        this.vehicleID = vehicleID;
+        this.vehicleState = vehicleState;
+        this.violationNumber = violationNumber;
+        this.violationZipCode = violationZipCode;
+
+    }
+
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public int getFineAmount() {
+        return fineAmount;
+    }
+
+    public String getViolationDescription() {
+        return violationDescription;
+    }
+
+    public int getVehicleID() {
+        return vehicleID;
+    }
+
+    public String getVehicleState() {
+        return vehicleState;
+    }
+
+    public int getViolationNumber() {
+        return violationNumber;
+    }
+
+    public int getViolationZipCode() {
+        return violationZipCode;
+    }
 
 }
