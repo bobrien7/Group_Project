34bbbd70fee4d1a504124e108a19c2b6945813be
dwlynch11 <dwlynch@seas.upenn.edu>
2020-04-21 07:00:28 -0400

    started pv csv reader class

--- src/edu/upenn/cit594/datamanagement/ParkingViolationCSVReader.java
+++ src/edu/upenn/cit594/datamanagement/ParkingViolationCSVReader.java
@@ -2,8 +2,12 @@ package edu.upenn.cit594.datamanagement;
 
 import edu.upenn.cit594.data.ParkingViolation;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Scanner;
 
 public class ParkingViolationCSVReader implements ParkingViolationReader{
 
@@ -14,13 +18,49 @@ public class ParkingViolationCSVReader implements ParkingViolationReader{
     }
 
 
+
+
     @Override
     public List<ParkingViolation> readParkingViolations() {
-        return null;
+        ArrayList<ParkingViolation> parkingViolation = new ArrayList<ParkingViolation>();
+        try {
+            Scanner scanner = new Scanner(new File(filename));
+
+            while(scanner.hasNextLine()) {
+
+                String violation = scanner.nextLine();
+                String[] violationData = violation.split(",");
+                String timestamp = violationData[0];
+                int fineAmount = Integer.parseInt(violationData[1]);
+                String violationDescription = violationData[2];
+                int vehicleID = Integer.parseInt(violationData[3]);
+                String vehicleState = violationData[4];
+                int violationNumber =Integer.parseInt(violationData[5]);
+                int violationZipCode =Integer.parseInt(violationData[6]);
+
+                parkingViolation.add(new ParkingViolation( timestamp, fineAmount, violationDescription, vehicleID, vehicleState, violationNumber, violationZipCode));
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
+        for(ParkingViolation pv : parkingViolation){
+            System.out.println(pv.getTimestamp());
+        }
+
+        return parkingViolation;
     }
 
     @Override
     public void logTimeAndFileName() {
 
     }
+    public static void main(String[] args) {
+        ParkingViolationCSVReader pvcsv = new ParkingViolationCSVReader("parking.csv");
+        pvcsv.readParkingViolations();
+
+    }
+
+
 }
