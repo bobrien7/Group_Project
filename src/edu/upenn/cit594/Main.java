b4dfcb8ea2a7a4cc3c5c3d3507ca4106aeb7ba5b
Bobby O'Brien <robrien7@seas.upenn.edu>
2020-04-26 14:53:05 -0400

    File authenticator uploaded

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -1,5 +1,6 @@
 package edu.upenn.cit594;
 
+import edu.upenn.cit594.data.FileAuthenticator;
 import edu.upenn.cit594.datamanagement.*;
 import edu.upenn.cit594.logging.Logger;
 import edu.upenn.cit594.processor.Processor;
@@ -14,29 +15,55 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int numberOfArguments = args.length;
+
+        if (numberOfArguments != 5) {
+
+            System.out.println("Error Occurred: Terminating Program");
+            System.exit(0);
+        }
+
         parkingFileFormat = args[0];         //"parking.csv";
         parkingFileName = args[1];
         propertyFileName = args[2];
         populationFileName = args[3];
         loggerFileName = args[4];
 
-        Logger.getInstance().log(Main.parkingFileFormat + " " + Main.parkingFileName + " " + Main.propertyFileName + " " + Main.populationFileName + " " + Main.loggerFileName);
-        //First thing we should do is log the current time and the runtime arguments 
+        FileAuthenticator fileAuthenticator = new FileAuthenticator(parkingFileFormat, parkingFileName, propertyFileName, populationFileName, loggerFileName);
+        Boolean fileCheckIndicator = fileAuthenticator.authenticate();
 
-        ParkingViolationCSVReader parkingFileReader = new ParkingViolationCSVReader(parkingFileName);
+        if (!fileCheckIndicator) {
 
-        PopulationDataReader populationReader = new PopulationDataReader(populationFileName);
+            System.out.println("Error Occurred: Terminating Program");
 
-        PropertyValueCSVReader propertyReader = new PropertyValueCSVReader(propertyFileName);
+        } else {
 
-        Processor processor = new Processor(parkingFileReader, populationReader, propertyReader);
+            ParkingViolationReader parkingViolationReader;
 
-        UserInterface ui = new UserInterface(processor);
+            if (parkingFileFormat.equalsIgnoreCase("CSV")) {
 
-        ui.start();
+                parkingViolationReader = new ParkingViolationCSVReader(parkingFileName);
+
+            } else {
+
+                parkingViolationReader = new ParkingViolationJSONReader(parkingFileName);
+
+            }
+
+            PopulationDataReader populationReader = new PopulationDataReader(populationFileName);
+            PropertyValueCSVReader propertyReader = new PropertyValueCSVReader(propertyFileName);
+
+            Logger.getInstance().log(Main.parkingFileFormat + " " + Main.parkingFileName + " " + Main.propertyFileName + " " + Main.populationFileName + " " + Main.loggerFileName);
+            //First thing we should do is log the current time and the runtime arguments
+
+            Processor processor = new Processor(parkingViolationReader, populationReader, propertyReader);
+
+            UserInterface ui = new UserInterface(processor);
+
+            ui.start();
+
+        }
 
     }
-
-
 
 }
--- src/edu/upenn/cit594/data/FileAuthenticator.java
+++ src/edu/upenn/cit594/data/FileAuthenticator.java
@@ -1,6 +1,80 @@
 package edu.upenn.cit594.data;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class FileAuthenticator {
+
+    String parkingFileFormat;
+    String parkingFileName;
+    String propertyFileName;
+    String populationFileName;
+    String loggerFileName;
+    int numberOfArguments;
+
+    public FileAuthenticator(String parkingFileFormat, String parkingFileName, String propertyFileName, String populationFileName, String loggerFileName) {
+
+        this.parkingFileFormat = parkingFileFormat;
+        this.parkingFileName = parkingFileName;
+        this.propertyFileName = propertyFileName;
+        this.populationFileName = populationFileName;
+        this.loggerFileName = loggerFileName;
+
+    }
+
+    public boolean authenticate() {
+
+        //this method will do all the checks to check the input files
+
+        parkingFileFormat = parkingFileFormat.toUpperCase();
+
+        if (parkingFileFormat.equals("CSV") || parkingFileFormat.equals("JSON")) {
+
+            //do nothing
+        } else {  //if this else statement is entered, that means file format wasn't text or json
+            return false;
+        }
+
+        File parkingFile = new File(parkingFileName);
+        File propertyFile = new File(propertyFileName);
+        File populationFile = new File(populationFileName);
+
+        if (!parkingFile.exists() || !propertyFile.exists() || !populationFile.exists()) {
+            System.out.println("One of the given files doesn't exist");
+            return false;
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(parkingFile));
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Couldn't open parking file");
+            return false;
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(propertyFile));
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Couldn't open properties file");
+            return false;
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(populationFile));
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Couldn't open population file");
+            return false;
+        }
+
+
+        return true;  //if the arguments check all of our tests... return true
+
+
+    }
 
 
 }
